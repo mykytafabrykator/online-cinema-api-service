3020d78e99966547417d34afb1a2a7a7f5631d71
Mykyta Fabrykator <dk01.fabrikator@lll.kpi.ua>
2025-03-12 15:45:24 +0200

    chore: initialize FastAPI application

---
+++ src/main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+
+app = FastAPI(
+    title="Online Cinema API",
+    description="An online cinema is a digital platform that allows users to "
+                "select, watch, and purchase access to movies and other video "
+                "materials via the internet. These services have become "
+                "popular due to their convenience, a wide selection of "
+                "content, and the ability to personalize the user experience.",
+)
+
+api_version_prefix = "/api/v1"
+
+
+@app.get("/")
+async def root():
+    return {"message": "Documentation may be found on /docs"}
