28871487956ef6ebca8bba50709132d5b27ad64c
Mykyta Fabrykator <dk01.fabrikator@lll.kpi.ua>
2025-03-13 17:50:47 +0200

    chore: configure database selection based on environment

--- src/database/__init__.py
+++ src/database/__init__.py
@@ -1,3 +1,5 @@
+import os
+
 from src.database.models.base import Base
 from src.database.models.accounts import (
     ActivationToken,
@@ -8,3 +10,16 @@ from src.database.models.accounts import (
     UserGroupEnum,
     UserProfile,
 )
+
+environment = os.getenv("ENVIRONMENT", "developing")
+
+if environment == "testing":
+    from src.database.session_sqlite import (
+        get_sqlite_db_contextmanager as get_db_contextmanager,
+        get_sqlite_db as get_db
+    )
+else:
+    from src.database.session_postgresql import (
+        get_postgresql_db_contextmanager as get_db_contextmanager,
+        get_postgresql_db as get_db
+    )
