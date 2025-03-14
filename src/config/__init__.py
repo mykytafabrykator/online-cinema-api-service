bb1e15dc0a392549f8dffca4884a1142440ce45c
Mykyta Fabrykator <dk01.fabrikator@lll.kpi.ua>
2025-03-14 16:49:55 +0200

    feat: add S3 storage client dependency

--- src/config/__init__.py
+++ src/config/__init__.py
@@ -7,4 +7,5 @@ from config.dependencies import (
     get_settings,
     get_jwt_auth_manager,
     get_accounts_email_notificator,
+    get_s3_storage_client,
 )
--- src/config/dependencies.py
+++ src/config/dependencies.py
@@ -6,6 +6,7 @@ from notifications import EmailSenderInterface, EmailSender
 from config import TestingSettings, Settings, BaseAppSettings
 from security import JWTAuthManagerInterface
 from security import JWTAuthManager
+from storages import S3StorageInterface, S3StorageClient
 
 
 def get_settings() -> BaseAppSettings:
@@ -90,4 +91,33 @@ def get_accounts_email_notificator(
         password_email_template_name=settings.PASSWORD_RESET_TEMPLATE_NAME,
         password_complete_email_template_name=settings.PASSWORD_RESET_COMPLETE_TEMPLATE_NAME,  # noqa: E501
         payment_success_email_template_name=settings.PAYMENT_SUCCESS_TEMPLATE_NAME,  # noqa: E501
+    )
+
+
+def get_s3_storage_client(
+    settings: BaseAppSettings = Depends(get_settings)
+) -> S3StorageInterface:
+    """
+    Retrieve an instance of the S3StorageInterface configured with the
+    application settings.
+
+    This function instantiates an S3StorageClient using the
+    provided settings, which include the S3 endpoint URL,
+    access credentials, and the bucket name. The returned client can be
+    used to interact with an S3-compatible
+    storage service for file uploads and URL generation.
+
+    Args:
+        settings (BaseAppSettings, optional): The application settings,
+        provided via dependency injection from `get_settings`.
+
+    Returns:
+        S3StorageInterface: An instance of S3StorageClient configured with
+        the appropriate S3 storage settings.
+    """
+    return S3StorageClient(
+        endpoint_url=settings.S3_STORAGE_ENDPOINT,
+        access_key=settings.S3_STORAGE_ACCESS_KEY,
+        secret_key=settings.S3_STORAGE_SECRET_KEY,
+        bucket_name=settings.S3_BUCKET_NAME
     )
