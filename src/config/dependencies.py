4eef6b8deaa556dd41d565738bdbd73e8a3d8b07
Mykyta Fabrykator <dk01.fabrikator@lll.kpi.ua>
2025-03-14 12:52:49 +0200

    feat: add email sender dependency for account notifications

--- src/config/dependencies.py
+++ src/config/dependencies.py
@@ -2,6 +2,7 @@ import os
 
 from fastapi import Depends
 
+from src.notifications import EmailSenderInterface, EmailSender
 from src.config.settings import TestingSettings, Settings, BaseAppSettings
 from src.security.interfaces import JWTAuthManagerInterface
 from src.security.token_manager import JWTAuthManager
@@ -30,8 +31,63 @@ def get_settings() -> BaseAppSettings:
 def get_jwt_auth_manager(
     settings: BaseAppSettings = Depends(get_settings),
 ) -> JWTAuthManagerInterface:
+    """
+    Create and return a JWT authentication manager instance.
+
+    This function uses the provided application settings to
+    instantiate a JWTAuthManager, which implements
+    the JWTAuthManagerInterface. The manager is configured with
+    secret keys for access and refresh tokens
+    as well as the JWT signing algorithm specified in the settings.
+
+    Args:
+        settings (BaseAppSettings, optional):
+        The application settings instance.
+        Defaults to the output of get_settings().
+
+    Returns:
+        JWTAuthManagerInterface: An instance of JWTAuthManager configured with
+        the appropriate secret keys and algorithm.
+    """
     return JWTAuthManager(
         secret_key_access=settings.SECRET_KEY_ACCESS,
         secret_key_refresh=settings.SECRET_KEY_REFRESH,
         algorithm=settings.JWT_SIGNING_ALGORITHM,
+    )
+
+
+def get_accounts_email_notificator(
+    settings: BaseAppSettings = Depends(get_settings)
+) -> EmailSenderInterface:
+    """
+    Retrieve an instance of the EmailSenderInterface
+    configured with the application settings.
+
+    This function creates an EmailSender using the provided settings,
+    which include details such as the email host,
+    port, credentials, TLS usage, and the directory and filenames for
+    email templates. This allows the application
+    to send various email notifications (e.g., activation, password reset)
+    as required.
+
+    Args:
+        settings (BaseAppSettings, optional): The application settings,
+        provided via dependency injection from `get_settings`.
+
+    Returns:
+        EmailSenderInterface: An instance of EmailSender
+        configured with the appropriate email settings.
+    """
+    return EmailSender(
+        hostname=settings.EMAIL_HOST,
+        port=settings.EMAIL_PORT,
+        email=settings.EMAIL_HOST_USER,
+        password=settings.EMAIL_HOST_PASSWORD,
+        use_tls=settings.EMAIL_USE_TLS,
+        template_dir=settings.PATH_TO_EMAIL_TEMPLATES_DIR,
+        activation_email_template_name=settings.ACTIVATION_EMAIL_TEMPLATE_NAME,
+        activation_complete_email_template_name=settings.ACTIVATION_COMPLETE_EMAIL_TEMPLATE_NAME,  # noqa: E501
+        password_email_template_name=settings.PASSWORD_RESET_TEMPLATE_NAME,
+        password_complete_email_template_name=settings.PASSWORD_RESET_COMPLETE_TEMPLATE_NAME,  # noqa: E501
+        payment_success_email_template_name=settings.PAYMENT_SUCCESS_TEMPLATE_NAME,  # noqa: E501
     )
