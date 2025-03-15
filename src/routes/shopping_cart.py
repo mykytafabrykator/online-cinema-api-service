5acf3b40ecc56c33c1ba32b4207b1aa04e83bdc2
Mykyta Fabrykator <dk01.fabrikator@lll.kpi.ua>
2025-03-15 21:09:40 +0200

    feat: add shopping cart list functionality

---
+++ src/routes/shopping_cart.py
@@ -0,0 +1,73 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from config import get_jwt_auth_manager
+from database import (
+    Order,
+    get_db
+)
+from database.crud.accounts import get_user_by_id
+from database.crud.movies import get_movie_by_id
+from database.crud.shopping_cart import (
+    add_cart_item,
+    create_cart,
+    create_order,
+    delete_cart_item,
+    delete_cart_item_by_cart,
+    get_cart_item,
+    get_cart_items_details,
+    get_purchased_movies_from_db,
+    get_user_cart,
+    process_order_payment_and_clear_cart,
+)
+from schemas.accounts import MessageResponseSchema
+from schemas.shopping_cart import (
+    CartCreate,
+    CartItemResponse,
+    CartResponse,
+    PurchasedMoviesResponse,
+)
+from security import JWTAuthManagerInterface
+from security.http import get_token
+from database.validators.shopping_cart import (
+    validate_movie_availability,
+    validate_not_in_cart,
+    validate_not_purchased,
+)
+
+router = APIRouter()
+
+
+@router.get(
+    "/",
+    response_model=CartResponse,
+    summary="Get user's shopping cart",
+    responses={
+        404: {"description": "User not found."},
+        401: {"description": "Unauthorized request."}
+    }
+)
+async def get_cart(
+        db: AsyncSession = Depends(get_db),
+        token: str = Depends(get_token),
+        jwt_manager: JWTAuthManagerInterface = Depends(get_jwt_auth_manager)
+) -> CartResponse:
+    token_data = jwt_manager.decode_access_token(token)
+    user_id = token_data["user_id"]
+
+    user = await get_user_by_id(db, user_id)
+    if not user:
+        raise HTTPException(
+            status_code=404,
+            detail="User with the given ID was not found."
+        )
+
+    cart = await get_user_cart(user, db)
+    if not cart:
+        return CartResponse(user_id=user.id, movies=[])
+
+    cart_items = await get_cart_items_details(cart, db)
+
+    return CartResponse(user_id=user.id, movies=cart_items)
+
+
