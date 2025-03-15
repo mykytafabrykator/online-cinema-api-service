725a0fec71dad0e7df9971cbbcce5d468cb1a53e
Mykyta Fabrykator <dk01.fabrikator@lll.kpi.ua>
2025-03-15 21:10:24 +0200

    feat: movies may be added to cart

--- src/routes/shopping_cart.py
+++ src/routes/shopping_cart.py
@@ -71,3 +71,49 @@ async def get_cart(
     return CartResponse(user_id=user.id, movies=cart_items)
 
 
+@router.post(
+    "/add/",
+    response_model=CartResponse,
+    summary="Add a movie to the shopping cart",
+    responses={
+        404: {"description": "User or movie not found."},
+        400: {"description": "Movie already in cart."},
+        401: {"description": "Unauthorized request."}
+    }
+)
+async def add_to_cart(
+        cart_data: CartCreate,
+        db: AsyncSession = Depends(get_db),
+        token: str = Depends(get_token),
+        jwt_manager: JWTAuthManagerInterface = Depends(get_jwt_auth_manager)
+) -> CartResponse:
+
+    token_data = jwt_manager.decode_access_token(token)
+    user_id = token_data["user_id"]
+
+    user = await get_user_by_id(db, user_id)
+    if not user:
+        raise HTTPException(
+            status_code=404,
+            detail="User with the given ID was not found."
+        )
+
+    movie = await get_movie_by_id(db, cart_data.movie_id)
+
+    if not movie:
+        raise HTTPException(status_code=404, detail="Movie not found")
+
+    await validate_movie_availability(movie)
+    await validate_not_in_cart(user, movie, db)
+    await validate_not_purchased(user, movie, db)
+
+    cart = await get_user_cart(user, db) or await create_cart(user, db)
+    await add_cart_item(cart, movie, db)
+    await db.refresh(cart)
+
+    cart_items = await get_cart_items_details(cart, db)
+
+    return CartResponse(user_id=user.id, movies=cart_items)
+
+
+
